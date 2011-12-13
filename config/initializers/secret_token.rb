ca2bca167ce12c2c4d5edfb9489eca5c0f624453
Kudder <Kidkudder@kudder.local>
2011-12-13 13:16:24 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixGunRomeo::Application.config.secret_token = '39bee15fa16729a9fc3b3b6e2791b79d7def70e3e9cda2c5d7d8005fad2929072ba09c3d53fc487dc030be7c27c798045ff9c6dd33e67c351e25ecc120c071da'
